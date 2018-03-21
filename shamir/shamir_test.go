3c855c85f8eba961ead23236a3e27963949d61d8
Noah <noahingham@protonmail.com>
2018-03-22 09:54:26 +1100

    Reverted shamir test edits

--- shamir/shamir_test.go
+++ shamir/shamir_test.go
@@ -78,7 +78,7 @@ var _ = Describe("Shamir's secret sharing", func() {
 			Ω(err).Should(BeNil())
 			Ω(int64(len(shares))).Should(Equal(N))
 			// For all K greater than, or equal to, 50 attempt to decode the secret.
-			for k := int64(50); k < 51; k++ {
+			for k := int64(50); k < 100; k++ {
 				// Pick K unique indices in the range [0, k).
 				indices := map[int]struct{}{}
 				for i := 0; i < int(k); i++ {
@@ -100,38 +100,38 @@ var _ = Describe("Shamir's secret sharing", func() {
 			}
 		})
 
-		// It("should return an incorrect secret from less than K shares", func() {
-		// 	// Shamir parameters.
-		// 	N := int64(100)
-		// 	K := int64(50)
-		// 	// prime, ok := big.NewInt(0).SetString("179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137859", 10)
-		// 	// Ω(ok).Should(Equal(true))
-		// 	secret := stackint.FromUint64(1234)
-		// 	// Split the secret.
-		// 	shares, err := Split(N, K, &prime, &secret)
-		// 	Ω(err).Should(BeNil())
-		// 	Ω(int64(len(shares))).Should(Equal(N))
-		// 	// For all K less than 50 attempt to decode the secret.
-		// 	for k := int64(1); k < 50; k++ {
-		// 		// Pick K unique indices in the range [0, k).
-		// 		indices := map[int]struct{}{}
-		// 		for i := 0; i < int(k); i++ {
-		// 			for {
-		// 				index := rand.Intn(int(k))
-		// 				if _, ok := indices[index]; !ok {
-		// 					indices[index] = struct{}{}
-		// 					break
-		// 				}
-		// 			}
-		// 		}
-		// 		// Use K shares to reconstruct the secret.
-		// 		kShares := make(Shares, k)
-		// 		for index := range indices {
-		// 			kShares[index] = shares[index]
-		// 		}
-		// 		decodedSecret := Join(&prime, kShares)
-		// 		Ω(decodedSecret).Should(Not(Equal(secret)))
-		// 	}
-		// })
+		It("should return an incorrect secret from less than K shares", func() {
+			// Shamir parameters.
+			N := int64(100)
+			K := int64(50)
+			// prime, ok := big.NewInt(0).SetString("179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137859", 10)
+			// Ω(ok).Should(Equal(true))
+			secret := stackint.FromUint64(1234)
+			// Split the secret.
+			shares, err := Split(N, K, &prime, &secret)
+			Ω(err).Should(BeNil())
+			Ω(int64(len(shares))).Should(Equal(N))
+			// For all K less than 50 attempt to decode the secret.
+			for k := int64(1); k < 50; k++ {
+				// Pick K unique indices in the range [0, k).
+				indices := map[int]struct{}{}
+				for i := 0; i < int(k); i++ {
+					for {
+						index := rand.Intn(int(k))
+						if _, ok := indices[index]; !ok {
+							indices[index] = struct{}{}
+							break
+						}
+					}
+				}
+				// Use K shares to reconstruct the secret.
+				kShares := make(Shares, k)
+				for index := range indices {
+					kShares[index] = shares[index]
+				}
+				decodedSecret := Join(&prime, kShares)
+				Ω(decodedSecret).Should(Not(Equal(secret)))
+			}
+		})
 	})
 })
