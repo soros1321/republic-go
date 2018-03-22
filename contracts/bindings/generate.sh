66ab76b817ba7f88320db3841af44fc3bb2bf1ad
susruth <susruth@republicprotocol.com>
2018-03-22 13:55:21 +1100

    Updated the generate file to generate gateway bindings

--- contracts/bindings/generate.sh
+++ contracts/bindings/generate.sh
@@ -1,7 +1,8 @@
 # Registrar
-abigen --sol ./eth-republic/contracts/DarkNodeRegistrar.sol -pkg bindings --out dnr.go
+# abigen --sol ./eth-republic/contracts/DarkNodeRegistrar.sol -pkg bindings --out dnr.go
+abigen --sol ./republic-sol/contracts/Gateway.sol -pkg bindings --out gateway.go
 
 # Atomic Swap
 # abigen --sol ./eth-atomic-swap/contracts/AtomicSwapEther.sol -pkg bindings --out AtomicSwapEth.go
 # abigen --sol ./eth-atomic-swap/contracts/AtomicSwapERC20.sol -pkg bindings --out AtomicSwapERC20.go
-abigen --sol ./eth-atomic-swap/contracts/TestERC20.sol -pkg bindings --out TestERC20.go
+# abigen --sol ./eth-atomic-swap/contracts/TestERC20.sol -pkg bindings --out TestERC20.go
